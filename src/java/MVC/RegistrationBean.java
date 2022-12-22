68954d8f21e0164b427b2708f31173271cc490eb
MATHEUS DO NASCIMENTO PEREIRA DA COSTA <matheusncosta2009@hotmail.com>
2022-12-22 16:46:53 -0300

    init

---
+++ src/java/MVC/RegistrationBean.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MVC;
+
+import javax.enterprise.context.RequestScoped;
+
+
+
+@Named
+@RequestScoped
+public class RegistrationBean {
+    private String salutation;
+	private String firstName;
+	private String lastName;
+	private Integer age;
+	private String email;
+
+//getters and setters omitted for brevity
+
+    public String getSalutation() {
+        return salutation;
+    }
+
+    public void setSalutation(String salutation) {
+        this.salutation = salutation;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
